d3498cf6ee0fd9e1b2aee135c8500bb00b1d3833
sergi <sergimasp@gmail.com>
2022-01-24 19:04:28 +0100

    Move Dijkstr's algorithm to a funciton

--- day15/puzzle2.py
+++ day15/puzzle2.py
@@ -1,8 +1,7 @@
 """
 Description:
-    The cavern is large, but has a very low ceiling, restricting your motion to two dimensions.
-    The shape of the cavern resembles a square; a quick scan of chiton density produces a map of risk level throughout
-    the cave (your puzzle input). For example:
+    The entire cave is actually five times larger in both dimensions than you thought; the area you originally scanned
+    is just one tile in a 5x5 tile area that forms the full map.
 
         1163751742
         1381373672
@@ -15,29 +14,37 @@ Description:
         1293138521
         2311944581
 
+    Your original map tile repeats to the right and downward; each time the tile repeats to the right or downward,
+    all of its risk levels are 1 higher than the tile immediately up or left of it.
+
+    However, risk levels above 9 wrap back around to 1. So, if your original map had some position with a risk level
+    of 8, then that same position on each of the 25 total tiles would be as follows:
+
+        8 9 1 2 3
+        9 1 2 3 4
+        1 2 3 4 5
+        2 3 4 5 6
+        3 4 5 6 7
+
+    Each single digit above corresponds to the example position with a value of 8 on the top-left tile. Because the full
+    map is actually five times larger in both dimensions, that position appears a total of 25 times, once in each
+    duplicated tile, with the values shown above.
+
 Goal:
 
-    You start in the top left position, your destination is the bottom right position, and you cannot move diagonally.
-    The number at each position is its risk level; to determine the total risk of an entire path, add up the risk levels
-    of each position you enter (that is, don't count the risk level of your starting position unless you enter it;
-    leaving it adds no risk to your total).
+    Equipped with the full map, you can now find a path from the top left corner to the bottom right corner with the
+    lowest total risk.
 
 Notes:
-    The best implementation for this problem is to use Dijkstra's algorithm or A*.
-
-    In this case, I am going to implement the Dijkstra's algorithm:
-        It is an algorithm for finding the shortest paths between nodes in a graph.
-
-        Dijkstra's original algorithm found the shortest path between two given nodes,[6] but a more common variant
-        fixes a single node as the "source" node and finds shortest paths from the source to all other nodes in the
-        graph, producing a shortest-path tree.
-
-    A* achieves better performance by using heuristics to guide its search.
+    1. The goal of this implementation is to use basic Python libraries. This makes the current implementation
+    no efficient as it should be. The problem come from the priority queue we are using,
+        a. Using a **heapq** the runtime should improve a lot.
 """
 
 import numpy as np
 import pandas as pd
 import math
+from dijkstra import dijkstra_algorith
 
 # ========= #
 # Functions #
@@ -63,7 +70,7 @@ def read_file_as_data_frame(path: str):
 # =================== #
 
 # Open the file conventionally,
-file = open_file('./input.txt')
+file = open_file('./input_simple.txt')
 #  read the entire file,
 # inputs = file.read()
 #  read the next line.
@@ -77,37 +84,20 @@ file = open_file('./input.txt')
 # ========== #
 
 starting_node = (0, 0)
+expansion_times = 5
 
 # ======= #
 # Helpers #
 # ======= #
 
 
-def get_neighbors(node: tuple, graph: dict) -> list:
-    """
-    Extract the neighbours of a node given the graph
+def translate_risk(risk, coord_x, coord_y, original_tile_width, original_tile_height):
+    x_translation = math.floor(coord_x / original_tile_width)
+    y_translation = math.floor(coord_y / original_tile_height)
 
-    Args:
-        node: Node of interest
-        graph: Dictionary describing the graph
+    risk_translation = (risk + x_translation + y_translation - 1) % 9 + 1
 
-    Returns:
-        List with all possible neighbours
-    """
-
-    result = []
-
-    for idx in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        neighbor_coord_y = node[0] + idx[0]
-        neighbor_coord_x = node[1] + idx[1]
-        if 0 <= neighbor_coord_y and 0 <= neighbor_coord_x:
-            if (neighbor_coord_y, neighbor_coord_x) in graph:
-                result.append((neighbor_coord_y, neighbor_coord_x))
-            else:
-                continue
-
-    return result
-
+    return risk_translation
 
 # ==== #
 # Main #
@@ -121,16 +111,24 @@ if __name__ == '__main__':
     # =============== #
 
     # Convert input data into a list of lists.
-    grid = []
+    tile = []
 
     line = file.readline()
     while line != '\n':
-        grid.append([int(risk) for risk in line.strip()])
+        tile.append([int(risk) for risk in line.strip()])
         line = file.readline()
 
-    # Previous lines equivalent to the following ones
-    # with open('input.txt') as f:
-    #     grid = [[int(risk) for risk in line.strip()] for line in f]
+    tile_width = len(tile[0])
+    grid_width = tile_width * 5
+    tile_height = len(tile)
+    grid_height = tile_height * 5
+
+    grid = [[] for y in range(grid_height)]
+
+    for y in range(grid_height):
+        for x in range(grid_width):
+            grid[y].append(
+                translate_risk(tile[y % tile_height][x % tile_width], x, y, tile_width, tile_height))
 
     # Close the file
     file.close()
@@ -142,47 +140,7 @@ if __name__ == '__main__':
     # Dijkstra's algorithm #
     # ==================== #
 
-    # 1. Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
-    unvisited_set = {}  # (node): tentative_distance
-    visited_set = {}  # (node): smallest distance from starting node
-
-    # 2. Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all
-    #    other nodes. The tentative distance of a node v is the length of the shortest path discovered so far between
-    #    the node v and the starting node.
-    for y in range(len(grid)):
-        row = grid[y]
-        for x in range(len(row)):
-            if (y, x) == starting_node:
-                unvisited_set[(y, x)] = 0
-            else:
-                unvisited_set[(y, x)] = math.inf
-    # 2. Set the initial node as current.
-    current_node = (0, 0)
-
-    # 3. For the current node, consider all of its unvisited neighbors and calculate their tentative distances through
-    #    the current node. Compare the newly calculated tentative distance to the current assigned value and assign the
-    #    smaller one.
-    while unvisited_set:
-        neighbors = get_neighbors(current_node, unvisited_set)
-
-        for (new_y, new_x) in neighbors:
-            tentative_distance = unvisited_set[current_node] + grid[new_y][new_x]
-
-            if tentative_distance < unvisited_set[(new_y, new_x)]:
-                unvisited_set[(new_y, new_x)] = tentative_distance
-
-        # 4. When we are done considering all of the unvisited neighbors of the current node, mark the current node
-        #    as visited and remove it from the unvisited set. A visited node will never be checked again.
-        visited_set[current_node] = unvisited_set[current_node]
-        del unvisited_set[current_node]
-
-        # 5. If the destination node has been marked visited (when planning a route between two specific nodes)
-        if end_node in visited_set:
-            break
-
-        # 6. Otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the
-        #    new current node, and go back to step 3.
-        current_node = min(unvisited_set, key=unvisited_set.get)
+    visited_set = dijkstra_algorith(grid, starting_node, end_node)
 
     # ================ #
     # Print the result #
